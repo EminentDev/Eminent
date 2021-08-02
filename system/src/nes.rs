15ca81e90aa546bf9a86f503c0f682de8bed48f2
Ray Redondo <rdrpenguin04@gmail.com>
2021-08-01 19:36:01 -0500

    Actual NES detection

--- system/src/nes.rs
+++ system/src/nes.rs
@@ -1,11 +1,19 @@
 use crate::{FileLoader, System};
 use common::ReadSeek;
+use std::io::SeekFrom;
 
 pub struct NesFileLoader {}
 
 impl FileLoader for NesFileLoader {
-    fn can_load(&self, _: &str, _: &mut dyn ReadSeek) -> bool {
-        true
+    fn can_load(&self, _: &str, file: &mut dyn ReadSeek) -> bool {
+        let mut buf: [u8; 4] = [0; 4];
+        if file.read_exact(&mut buf).is_err() {
+            return false;
+        }
+        if file.seek(SeekFrom::Start(0)).is_err() {
+            return false;
+        }
+        buf[0] == b'N' && buf[1] == b'E' && buf[2] == b'S' && buf[3] == 0x1A
     }
 
     fn load(&self, file: &mut dyn ReadSeek) -> Box<dyn System> {
