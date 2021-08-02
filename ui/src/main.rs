a365f9ac1c7c66a4bc30a60aace714daa5e173c9
Ray Redondo <rdrpenguin04@gmail.com>
2021-08-01 19:48:19 -0500

    Make the UI do anything

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let mut file = File::open(filename)?;
+    let loader = system::get_file_loader(filename, &mut file).unwrap();
+    let _ = loader.load(&mut file);
+    Ok(())
 }
