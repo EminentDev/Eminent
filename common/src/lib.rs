feeeec102705f023ee27b0dd4a482ce5b6e7d78b
Ray Redondo <rdrpenguin04@gmail.com>
2021-08-03 22:34:27 -0500

    WIP Bus system

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,3 +2,18 @@ use std::io::{Read, Seek};
 
 pub trait ReadSeek: Read + Seek {}
 impl<T: Read + Seek + ?Sized> ReadSeek for T {}
+
+pub enum BusResult {
+    Hit,
+    Fault,
+    OpenBus,
+}
+
+pub trait BusDevice {
+    fn read(&mut self, addr: usize, value: &mut [u8]) -> BusResult;
+    fn write(&mut self, addr: usize, value: &[u8]) -> BusResult; // OpenBus here is equivalent to Fault
+}
+
+pub struct Bus<'a> {
+    devices: Vec<&'a mut dyn BusDevice>,
+}
