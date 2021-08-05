7fbb787ad0e3bfe0a55e580a5e50dfd487a33a6b
Ray Redondo <rdrpenguin04@gmail.com>
2021-08-04 20:30:50 -0500

    Bus API update

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,17 +3,30 @@ use std::io::{Read, Seek};
 pub trait ReadSeek: Read + Seek {}
 impl<T: Read + Seek + ?Sized> ReadSeek for T {}
 
-pub enum BusResult {
+pub enum BusStatus {
     Hit,
-    Fault,
     OpenBus,
 }
 
-pub trait BusDevice {
-    fn read(&mut self, addr: usize, value: &mut [u8]) -> BusResult;
-    fn write(&mut self, addr: usize, value: &[u8]) -> BusResult; // OpenBus here is equivalent to Fault
+pub trait BusDevice<F, const WIDTH: usize> {
+    fn read(&mut self, addr: usize, value: &mut [u8; WIDTH]) -> (Option<F>, [BusStatus; WIDTH]);
+    fn write(&mut self, addr: usize, value: &[u8; WIDTH]) -> Option<F>;
 }
 
-pub struct Bus<'a> {
-    devices: Vec<&'a mut dyn BusDevice>,
+pub trait BusTransform {
+    fn transform(&self, addr: usize) -> usize;
+}
+
+pub struct Bus<'a, F, const WIDTH: usize> {
+    devices: Vec<(&'a mut dyn BusDevice<F, WIDTH>, &'a dyn BusTransform)>,
+}
+
+impl<'a, F, const WIDTH: usize> Bus<'a, F, WIDTH> {
+    pub fn read(_addr: usize) -> (Option<F>, Box<[u8]>) {
+        (None, Vec::with_capacity(WIDTH).into_boxed_slice())
+    }
+
+    pub fn write(_addr: usize, _value: &[u8; WIDTH]) -> Option<F> {
+        None
+    }
 }
