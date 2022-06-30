f7ca036f98a6905ea2dd44a80b4a8c4e4959cd2d
Ray Redondo <rdrpenguin04@gmail.com>
2022-06-30 16:39:41 +0000

    chore: rustfmt

--- processor/src/m68k.rs
+++ processor/src/m68k.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{cell::RefCell, rc::Rc};
 
 use common::Bus;
 
@@ -54,11 +54,13 @@ impl M68KEffectiveAddress {
         D::try_from(match (self.size, self.m, self.xn) {
             (M68KSize::Byte, 0, xn) => m68k.d[xn as usize] & 0xFF,
             (a, b, c) => todo!("{:?}, {}, {}", a, b, c),
-        }).ok().unwrap()
+        })
+        .ok()
+        .unwrap()
     }
 
     pub fn write<D: Into<u32>>(&self, m68k: &mut M68K, data: D) {
-        match(self.size, self.m, self.xn) {
+        match (self.size, self.m, self.xn) {
             (a, b, c) => todo!("{:?}, {}, {}", a, b, c),
         }
     }
@@ -81,12 +83,18 @@ impl M68K {
         let ssp: u32 = unsafe {
             let ms = bus.borrow().read(0).1;
             let ls = bus.borrow().read(2).1;
-            u32::from(ms[0]) << 24 | u32::from(ms[1]) << 16 | u32::from(ls[0]) << 8 | u32::from(ls[1])
+            u32::from(ms[0]) << 24
+                | u32::from(ms[1]) << 16
+                | u32::from(ls[0]) << 8
+                | u32::from(ls[1])
         };
         let pc: u32 = unsafe {
             let ms = bus.borrow().read(4).1;
             let ls = bus.borrow().read(6).1;
-            u32::from(ms[0]) << 24 | u32::from(ms[1]) << 16 | u32::from(ls[0]) << 8 | u32::from(ls[1])
+            u32::from(ms[0]) << 24
+                | u32::from(ms[1]) << 16
+                | u32::from(ls[0]) << 8
+                | u32::from(ls[1])
         };
         Self {
             bus,
@@ -102,8 +110,9 @@ impl M68K {
     fn read_u8(&mut self, addr: u32) -> u8 {
         let addr = usize::try_from(addr).unwrap();
         let result = unsafe { self.bus.borrow().read(addr & 0xFFFFFFFE) };
-        if result.0.is_some() { todo!(); }
-        else if addr & 1 == 0 {
+        if result.0.is_some() {
+            todo!();
+        } else if addr & 1 == 0 {
             result.1[0]
         } else {
             result.1[1]
@@ -113,8 +122,9 @@ impl M68K {
     fn read_u16(&mut self, addr: u32) -> u16 {
         let addr = usize::try_from(addr).unwrap();
         let result = unsafe { self.bus.borrow().read(addr & 0xFFFFFFFE) };
-        if result.0.is_some() { todo!(); }
-        else {
+        if result.0.is_some() {
+            todo!();
+        } else {
             u16::from(result.1[0]) << 8 | u16::from(result.1[1])
         }
     }
@@ -122,11 +132,17 @@ impl M68K {
     fn read_u32(&mut self, addr: u32) -> u32 {
         let addr = usize::try_from(addr).unwrap();
         let result1 = unsafe { self.bus.borrow().read(addr & 0xFFFFFFFE) };
-        if result1.0.is_some() { todo!(); }
+        if result1.0.is_some() {
+            todo!();
+        }
         let result2 = unsafe { self.bus.borrow().read(addr & 0xFFFFFFFE) };
-        if result2.0.is_some() { todo!(); }
-        else {
-            u32::from(result1.1[0]) << 24 | u32::from(result1.1[1]) << 16 | u32::from(result2.1[0]) << 8 | u32::from(result2.1[1])
+        if result2.0.is_some() {
+            todo!();
+        } else {
+            u32::from(result1.1[0]) << 24
+                | u32::from(result1.1[1]) << 16
+                | u32::from(result2.1[0]) << 8
+                | u32::from(result2.1[1])
         }
     }
 }
@@ -136,50 +152,57 @@ impl Processor for M68K {
         let insn = self.read_u16(self.pc);
         self.pc += 2;
         match insn & 0xF000 {
-            0 => if insn & 0x0100 == 0 {
-                match insn & 0x0E00 {
-                    0x0200 => {
-                        if insn == 0x023C || insn == 0x023C {
-                            todo!("ANDI to SR");
-                        }
-                        let size = M68KSize::from_type_a((insn & 0x00C0) >> 6);
-                        let ea = M68KEffectiveAddress::new(size, (insn & 0x0038) >> 3, insn & 0x0007);
-                        let a: u32 = ea.read(self);
-                        let b = match size {
-                            M68KSize::Byte => {
-                                let result = self.read_u8(self.pc);
-                                self.pc += 2;
-                                result.into()
+            0 => {
+                if insn & 0x0100 == 0 {
+                    match insn & 0x0E00 {
+                        0x0200 => {
+                            if insn == 0x023C || insn == 0x023C {
+                                todo!("ANDI to SR");
                             }
-                            M68KSize::Word => {
-                                let result = self.read_u16(self.pc);
-                                self.pc += 2;
-                                result.into()
+                            let size = M68KSize::from_type_a((insn & 0x00C0) >> 6);
+                            let ea = M68KEffectiveAddress::new(
+                                size,
+                                (insn & 0x0038) >> 3,
+                                insn & 0x0007,
+                            );
+                            let a: u32 = ea.read(self);
+                            let b = match size {
+                                M68KSize::Byte => {
+                                    let result = self.read_u8(self.pc);
+                                    self.pc += 2;
+                                    result.into()
+                                }
+                                M68KSize::Word => {
+                                    let result = self.read_u16(self.pc);
+                                    self.pc += 2;
+                                    result.into()
+                                }
+                                M68KSize::Long => {
+                                    let result = self.read_u32(self.pc);
+                                    self.pc += 2;
+                                    result
+                                }
+                            };
+                            let result = a & b;
+                            self.sr &= 0xFFF0;
+                            if result == 0 {
+                                self.sr |= 4;
                             }
-                            M68KSize::Long => {
-                                let result = self.read_u32(self.pc);
-                                self.pc += 2;
-                                result
+                            if match size {
+                                M68KSize::Byte => result & 0x80,
+                                M68KSize::Word => result & 0x8000,
+                                M68KSize::Long => result & 0x80000000,
+                            } != 0
+                            {
+                                self.sr |= 8;
                             }
-                        };
-                        let result = a & b;
-                        self.sr &= 0xFFF0;
-                        if result == 0 {
-                            self.sr |= 4;
+                            ea.write(self, result);
                         }
-                        if match size {
-                            M68KSize::Byte => result & 0x80,
-                            M68KSize::Word => result & 0x8000,
-                            M68KSize::Long => result & 0x80000000,
-                        } != 0 {
-                            self.sr |= 8;
-                        }
-                        ea.write(self, result);
+                        _ => todo!("0x{:04X}", insn),
                     }
-                    _ => todo!("0x{:04X}", insn),
+                } else {
+                    todo!("0x01__");
                 }
-            } else {
-                todo!("0x01__");
             }
             x if x & 0xC000 == 0 => todo!("MOVE instructions"),
             x => todo!("0x{:04X}", x),
