7105ae5c6763a1e3a2e6dd11386e027cdc0e8609
Ray Redondo <rdrpenguin04@gmail.com>
2022-06-30 21:09:57 +0000

    m68k: more instructions and addressing modes

--- processor/src/m68k.rs
+++ processor/src/m68k.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc, fmt};
+use std::{cell::RefCell, fmt, rc::Rc, result};
 
 use common::Bus;
 
@@ -25,6 +25,15 @@ impl M68KSize {
             _ => panic!("invalid size"),
         }
     }
+
+    pub fn from_type_b<A: TryInto<u8>>(size: A) -> Self {
+        match size.try_into().ok().unwrap() {
+            1 => M68KSize::Byte,
+            3 => M68KSize::Word,
+            2 => M68KSize::Long,
+            _ => panic!("invalid size"),
+        }
+    }
 }
 
 impl fmt::Display for M68KSize {
@@ -33,7 +42,8 @@ impl fmt::Display for M68KSize {
             M68KSize::Byte => ".b",
             M68KSize::Word => ".w",
             M68KSize::Long => ".l",
-        }.fmt(f)
+        }
+        .fmt(f)
     }
 }
 
@@ -67,6 +77,14 @@ impl M68KEffectiveAddress {
 
     pub fn resolve(&mut self, m68k: &mut M68K) {
         match self.m {
+            3 => {
+                let val = m68k.a(self.xn);
+                *m68k.a_mut(self.xn) += 1;
+                if DEBUG {
+                    println!("Resolving (A{})+ as {:08X}", self.xn, val);
+                }
+                self.resolution = Some(val);
+            }
             5 => {
                 // Sign extension in Rust be like:
                 let disp = m68k.read_u16(m68k.pc) as i16 as i32 as u32;
@@ -77,7 +95,41 @@ impl M68KEffectiveAddress {
                 }
                 self.resolution = Some(addr);
             }
-            6 | 7 => todo!(),
+            6 => todo!(),
+            7 => match self.xn {
+                1 => {
+                    let val = m68k.read_u32(m68k.pc);
+                    m68k.pc += 4;
+                    if DEBUG {
+                        println!("Resolving 0x{:08X}", val);
+                    }
+                    self.resolution = Some(val);
+                }
+                4 => {
+                    let val = match self.size {
+                        M68KSize::Byte => {
+                            let result = m68k.read_u8(m68k.pc);
+                            m68k.pc += 2;
+                            result.into()
+                        }
+                        M68KSize::Word => {
+                            let result = m68k.read_u16(m68k.pc);
+                            m68k.pc += 2;
+                            result.into()
+                        }
+                        M68KSize::Long => {
+                            let result = m68k.read_u32(m68k.pc);
+                            m68k.pc += 4;
+                            result
+                        }
+                    };
+                    if DEBUG {
+                        println!("Resolving immediate #0x{:08X}", val);
+                    }
+                    self.resolution = Some(val);
+                }
+                x => todo!("{}", x),
+            },
             _ => {}
         }
     }
@@ -85,7 +137,9 @@ impl M68KEffectiveAddress {
     pub fn read<D: TryFrom<u32>>(&self, m68k: &mut M68K) -> D {
         D::try_from(match (self.size, self.m, self.xn) {
             (M68KSize::Byte, 0, xn) => m68k.d[xn as usize] & 0xFF,
+            (M68KSize::Word, 3, _) => m68k.read_u16(self.resolution.unwrap()) as u32,
             (M68KSize::Long, 5, _) => m68k.read_u32(self.resolution.unwrap()),
+            (_, 7, 4) => self.resolution.unwrap(),
             (a, b, c) => todo!("{:?}, {}, {}", a, b, c),
         })
         .ok()
@@ -94,8 +148,18 @@ impl M68KEffectiveAddress {
 
     pub fn write<D: Into<u32>>(&self, m68k: &mut M68K, data: D) {
         match (self.size, self.m, self.xn) {
+            (M68KSize::Word, 3, _) | (M68KSize::Word, 7, 1) => {
+                m68k.write_u16(self.resolution.unwrap(), data.into() as u16)
+            }
             (M68KSize::Long, 5, _) => m68k.write_u32(self.resolution.unwrap(), data.into()),
             (a, b, c) => todo!("{:?}, {}, {}", a, b, c),
+        }
+    }
+
+    pub fn ea(&self) -> u32 {
+        match (self.m, self.xn) {
+            (7, 1) => self.resolution.unwrap(),
+            (a, b) => todo!("{}, {}", a, b),
         }
     }
 }
@@ -119,6 +183,14 @@ impl M68K {
             todo!()
         } else {
             self.a[an]
+        }
+    }
+    fn a_mut<X: Into<usize>>(&mut self, an: X) -> &mut u32 {
+        let an = an.into();
+        if an == 7 {
+            todo!()
+        } else {
+            &mut self.a[an]
         }
     }
 
@@ -188,7 +260,6 @@ impl M68K {
         }
         let result1 = result1.1.clone();
         let result2 = unsafe { self.bus.borrow().read((addr + 2) & 0xFFFFFFFE) };
-        println!("{:?} {:?}", result1, result2.1);
         let result = if result2.0.is_some() {
             todo!();
         } else {
@@ -270,7 +341,10 @@ impl Processor for M68K {
                             };
                             let result = a & b;
                             if DEBUG {
-                                println!("ANDI{} #0x{:08X}, 0x{:08X} = 0x{:08X}", size, b, a, result);
+                                println!(
+                                    "ANDI{} #0x{:08X}, 0x{:08X} = 0x{:08X}",
+                                    size, b, a, result
+                                );
                             }
                             self.sr &= 0xFFF0;
                             if result == 0 {
@@ -292,7 +366,54 @@ impl Processor for M68K {
                     todo!("0x01__");
                 }
             }
-            x if x & 0xC000 == 0 => todo!("MOVE instructions"),
+            x if x & 0xC000 == 0 => {
+                let size = M68KSize::from_type_b((insn & 0x3000) >> 12);
+                let mut dest_ea =
+                    M68KEffectiveAddress::new(size, (insn & 0x01C0) >> 6, (insn & 0x0E00) >> 9);
+                let mut src_ea =
+                    M68KEffectiveAddress::new(size, (insn & 0x0038) >> 3, insn & 0x0007);
+                src_ea.resolve(self);
+                let val: u32 = src_ea.read(self);
+                dest_ea.resolve(self);
+                dest_ea.write(self, val);
+                println!("MOVE{} 0x{:08X}, <TODO>", size, val);
+            }
+            0x4000 => match insn {
+                x if x & 0x0FC0 == 0x06C0 => {
+                    let mut ea = M68KEffectiveAddress::new(1, (insn & 0x0038) >> 3, insn & 0x0007);
+                    ea.resolve(self);
+                    self.sr = ea.read(self);
+                    if DEBUG {
+                        println!("MOVE 0x{:04X}, SR", self.sr);
+                    }
+                }
+                x if x & 0x01C0 == 0x01C0 => {
+                    let mut ea = M68KEffectiveAddress::new(2, (insn & 0x0038) >> 3, insn & 0x0007);
+                    ea.resolve(self);
+                    let an = (insn & 0x0E00) >> 9;
+                    let ea = ea.ea();
+                    *self.a_mut(an) = ea;
+                    if DEBUG {
+                        println!("LEA 0x{:08X}, A{}", ea, an);
+                    }
+                }
+                x => todo!("0x{:04X}", x),
+            },
+            0x5000 => match insn {
+                x if x & 0x00F8 == 0x00C8 => {
+                    todo!("To future me: condition codes :)");
+                }
+                x => todo!("0x{:04X}", x),
+            },
+            0x7000 => {
+                assert!(insn & 0x0100 == 0);
+                let val: u32 = (insn & 0xFF) as i8 as i64 as u32;
+                let dn = (insn & 0x0E00) >> 9;
+                self.d[dn as usize] = val;
+                if DEBUG {
+                    println!("MOVEQ 0x{:08X}, D{}", val, dn);
+                }
+            }
             x => todo!("0x{:04X}", x),
         }
     }
