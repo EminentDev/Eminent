60276d9bd12f7922d613d760c7433d4f9d51ea7c
Ray Redondo <rdrpenguin04@gmail.com>
2022-07-05 19:53:32 +0000

    m68k: add cc and more instructions

--- processor/src/m68k.rs
+++ processor/src/m68k.rs
@@ -58,6 +58,73 @@ impl From<M68KSize> for u8 {
     }
 }
 
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum M68KConditionCode {
+    True,
+    False,
+    High,
+    LowOrSame,
+    CarryClear,
+    CarrySet,
+    NotEqual,
+    Equal,
+    OverflowClear,
+    OverflowSet,
+    Plus,
+    Minus,
+    GreaterOrEqual,
+    LessThan,
+    GreaterThan,
+    LessOrEqual,
+}
+
+impl M68KConditionCode {
+    pub fn eval(&self, m68k: &M68K) -> bool {
+        match self {
+            Self::True => true,
+            Self::False => false,
+            Self::CarryClear => m68k.sr & 1 == 0,
+            Self::CarrySet => m68k.sr & 1 != 0,
+            x => todo!("{:?}", x),
+        }
+    }
+}
+
+impl fmt::Display for M68KConditionCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::True => "T",
+            Self::False => "F",
+            _ => todo!(),
+        }
+        .fmt(f)
+    }
+}
+
+impl From<u8> for M68KConditionCode {
+    fn from(cc: u8) -> Self {
+        match cc {
+            0 => Self::True,
+            1 => Self::False,
+            2 => Self::High,
+            3 => Self::LowOrSame,
+            4 => Self::CarryClear,
+            5 => Self::CarrySet,
+            6 => Self::NotEqual,
+            7 => Self::Equal,
+            8 => Self::OverflowClear,
+            9 => Self::OverflowSet,
+            10 => Self::Plus,
+            11 => Self::Minus,
+            12 => Self::GreaterOrEqual,
+            13 => Self::LessThan,
+            14 => Self::GreaterThan,
+            15 => Self::LessOrEqual,
+            _ => unreachable!(),
+        }
+    }
+}
+
 pub struct M68KEffectiveAddress {
     size: M68KSize,
     m: u8,
@@ -136,7 +203,9 @@ impl M68KEffectiveAddress {
 
     pub fn read<D: TryFrom<u32>>(&self, m68k: &mut M68K) -> D {
         D::try_from(match (self.size, self.m, self.xn) {
-            (M68KSize::Byte, 0, xn) => m68k.d[xn as usize] & 0xFF,
+            (M68KSize::Byte, 0, dn) => m68k.d[dn as usize] & 0xFF,
+            (M68KSize::Word, 0, dn) => m68k.d[dn as usize] & 0xFFFF,
+            (M68KSize::Long, 0, dn) => m68k.d[dn as usize],
             (M68KSize::Word, 3, _) => m68k.read_u16(self.resolution.unwrap()) as u32,
             (M68KSize::Long, 5, _) => m68k.read_u32(self.resolution.unwrap()),
             (_, 7, 4) => self.resolution.unwrap(),
@@ -148,8 +217,24 @@ impl M68KEffectiveAddress {
 
     pub fn write<D: Into<u32>>(&self, m68k: &mut M68K, data: D) {
         match (self.size, self.m, self.xn) {
+            (M68KSize::Word, 0, dn) => {
+                let dn = dn as usize;
+                let mut val = m68k.d[dn];
+                val &= 0xFF00;
+                val |= data.into();
+                m68k.d[dn] = val;
+            }
+            (M68KSize::Long, 0, dn) => {
+                m68k.d[dn as usize] = data.into();
+            }
+            (M68KSize::Long, 1, an) => {
+                m68k.a[an as usize] = data.into();
+            }
             (M68KSize::Word, 3, _) | (M68KSize::Word, 7, 1) => {
                 m68k.write_u16(self.resolution.unwrap(), data.into() as u16)
+            }
+            (M68KSize::Long, 3, _) | (M68KSize::Long, 7, 1) => {
+                m68k.write_u32(self.resolution.unwrap(), data.into())
             }
             (M68KSize::Long, 5, _) => m68k.write_u32(self.resolution.unwrap(), data.into()),
             (a, b, c) => todo!("{:?}, {}, {}", a, b, c),
@@ -159,6 +244,19 @@ impl M68KEffectiveAddress {
     pub fn ea(&self) -> u32 {
         match (self.m, self.xn) {
             (7, 1) => self.resolution.unwrap(),
+            (a, b) => todo!("{}, {}", a, b),
+        }
+    }
+}
+
+impl fmt::Display for M68KEffectiveAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match (self.m, self.xn) {
+            (0, dn) => write!(f, "D{}", dn),
+            (1, an) => write!(f, "A{}", an),
+            (3, an) => write!(f, "(A{})+", an),
+            (7, 1) => write!(f, "0x{:08X}", self.resolution.unwrap()),
+            (7, 4) => write!(f, "#0x{:08X}", self.resolution.unwrap()),
             (a, b) => todo!("{}, {}", a, b),
         }
     }
@@ -340,12 +438,6 @@ impl Processor for M68K {
                                 }
                             };
                             let result = a & b;
-                            if DEBUG {
-                                println!(
-                                    "ANDI{} #0x{:08X}, 0x{:08X} = 0x{:08X}",
-                                    size, b, a, result
-                                );
-                            }
                             self.sr &= 0xFFF0;
                             if result == 0 {
                                 self.sr |= 4;
@@ -359,6 +451,53 @@ impl Processor for M68K {
                                 self.sr |= 8;
                             }
                             ea.write(self, result);
+                            if DEBUG {
+                                println!("ANDI{} #0x{:08X}, {} = 0x{:08X}", size, b, ea, result);
+                            }
+                        }
+                        0x0600 => {
+                            let size = M68KSize::from_type_a((insn & 0x00C0) >> 6);
+                            let mut ea = M68KEffectiveAddress::new(
+                                size,
+                                (insn & 0x0038) >> 3,
+                                insn & 0x0007,
+                            );
+                            ea.resolve(self);
+                            let a: u32 = ea.read(self);
+                            let b = match size {
+                                M68KSize::Byte => {
+                                    let result = self.read_u8(self.pc);
+                                    self.pc += 2;
+                                    result.into()
+                                }
+                                M68KSize::Word => {
+                                    let result = self.read_u16(self.pc);
+                                    self.pc += 2;
+                                    result.into()
+                                }
+                                M68KSize::Long => {
+                                    let result = self.read_u32(self.pc);
+                                    self.pc += 4;
+                                    result
+                                }
+                            };
+                            let result = a.wrapping_add(b);
+                            self.sr &= 0xFFF0;
+                            if result == 0 {
+                                self.sr |= 4;
+                            }
+                            if match size {
+                                M68KSize::Byte => result & 0x80,
+                                M68KSize::Word => result & 0x8000,
+                                M68KSize::Long => result & 0x80000000,
+                            } != 0
+                            {
+                                self.sr |= 8;
+                            }
+                            ea.write(self, result);
+                            if DEBUG {
+                                println!("ADDI{} #0x{:08X}, {} = 0x{:08X}", size, b, ea, result);
+                            }
                         }
                         _ => todo!("0x{:04X}", insn),
                     }
@@ -376,7 +515,9 @@ impl Processor for M68K {
                 let val: u32 = src_ea.read(self);
                 dest_ea.resolve(self);
                 dest_ea.write(self, val);
-                println!("MOVE{} 0x{:08X}, <TODO>", size, val);
+                if DEBUG {
+                    println!("MOVE{} {}, {}", size, dest_ea, src_ea);
+                }
             }
             0x4000 => match insn {
                 x if x & 0x0FC0 == 0x06C0 => {
@@ -384,7 +525,15 @@ impl Processor for M68K {
                     ea.resolve(self);
                     self.sr = ea.read(self);
                     if DEBUG {
-                        println!("MOVE 0x{:04X}, SR", self.sr);
+                        println!("MOVE {}, SR", ea);
+                    }
+                }
+                x if x & 0x0F00 == 0x0200 => {
+                    let mut ea = M68KEffectiveAddress::new(1, (insn & 0x0038) >> 3, insn & 0x0007);
+                    ea.resolve(self);
+                    ea.write(self, 0u32);
+                    if DEBUG {
+                        println!("CLR {}", ea);
                     }
                 }
                 x if x & 0x01C0 == 0x01C0 => {
@@ -401,17 +550,84 @@ impl Processor for M68K {
             },
             0x5000 => match insn {
                 x if x & 0x00F8 == 0x00C8 => {
-                    todo!("To future me: condition codes :)");
+                    let next_pc = self
+                        .pc
+                        .wrapping_add(self.read_u16(self.pc) as i16 as i32 as u32);
+                    self.pc += 2;
+                    let dn = (x & 0x0007) as usize;
+                    let cc = M68KConditionCode::from(((x & 0x0F00) >> 8) as u8);
+                    if !cc.eval(self) {
+                        let mut val = self.d[dn] as u16;
+                        val = val.wrapping_sub(1);
+                        self.d[dn] = (self.d[dn] & 0xFF00) | val as u32;
+                        if val != 0xFFFF {
+                            self.pc = next_pc;
+                        }
+                    }
+                    if DEBUG {
+                        println!("DB{} D{}, 0x{:04X}", cc, dn, next_pc);
+                    }
+                }
+                x if x & 0x00C0 == 0x00C0 => todo!("Scc"),
+                x if x & 0x0100 == 0 => {
+                    let size = M68KSize::from_type_a((insn & 0x00C0) >> 6);
+                    let mut ea =
+                        M68KEffectiveAddress::new(size, (insn & 0x0038) >> 3, insn & 0x0007);
+                    ea.resolve(self);
+                    let a: u32 = ea.read(self);
+                    let mut b = ((insn & 0x0E00) >> 9) as u32;
+                    if b == 0 {
+                        b = 8;
+                    }
+                    let result = a.wrapping_add(b);
+                    self.sr &= 0xFFF0;
+                    if result == 0 {
+                        self.sr |= 4;
+                    }
+                    if match size {
+                        M68KSize::Byte => result & 0x80,
+                        M68KSize::Word => result & 0x8000,
+                        M68KSize::Long => result & 0x80000000,
+                    } != 0
+                    {
+                        self.sr |= 8;
+                    }
+                    ea.write(self, result);
+                    if DEBUG {
+                        println!("ADDQ{} #0x{:01X}, {} = 0x{:08X}", size, b, ea, result);
+                    }
                 }
                 x => todo!("0x{:04X}", x),
             },
+            0x6000 => {
+                if insn & 0x0F00 == 0x0100 {
+                    todo!("BSR");
+                }
+                let old_pc = self.pc;
+                let mut disp = (insn & 0xFF) as u8 as i8 as i32 as u32;
+                if disp == 0 {
+                    disp = self.read_u16(self.pc) as i16 as i32 as u32;
+                    self.pc += 2;
+                } else if disp == 0xFFFFFFFF {
+                    disp = self.read_u32(self.pc);
+                    self.pc += 4;
+                }
+                let cc = M68KConditionCode::from(((insn & 0x0F00) >> 8) as u8);
+                let next_pc = old_pc.wrapping_add(disp);
+                if cc.eval(self) {
+                    self.pc = next_pc;
+                }
+                if DEBUG {
+                    println!("B{} 0x{:08X}", cc, next_pc);
+                }
+            }
             0x7000 => {
                 assert!(insn & 0x0100 == 0);
                 let val: u32 = (insn & 0xFF) as i8 as i64 as u32;
                 let dn = (insn & 0x0E00) >> 9;
                 self.d[dn as usize] = val;
                 if DEBUG {
-                    println!("MOVEQ 0x{:08X}, D{}", val, dn);
+                    println!("MOVEQ #0x{:08X}, D{}", val, dn);
                 }
             }
             x => todo!("0x{:04X}", x),
